fe3fd8c3fa2aeb8679757ed6be8d756c4bd7f605
James Cox <j-cox03@hotmail.com>
2017-07-31 21:21:50 +0100

    Made method to add a single cat

--- public/app.js
+++ public/app.js
@@ -10,6 +10,15 @@ function addCat(){
   listElements[1] = document.createElement("li");
   listElements[2] = document.createElement("li");
 
+  listElements[0].innerHTML = "Name: Billy";
+  listElements[1].innerHTML = "Favourite Food: ice cream";
+
+  var catImage = document.createElement("img");
+  catImage.setAttribute("src", "https://www.petfinder.com/wp-content/uploads/2012/11/99233806-bringing-home-new-cat-632x475.jpg");
+  catImage.setAttribute("width", "500");
+
+  listElements[2].appendChild(catImage);
+
   for(var element of listElements){
     unorderedList.appendChild(element);
   }
